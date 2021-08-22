ba6e306de53df09882e1dab24d897d0648c1ec96
Anvit Garg <anvit25@gmail.com>
2021-08-22 17:28:32 +0530

    Refactored, Updated 8,9,10

--- app/Day10.hs
+++ app/Day10.hs
@@ -1,26 +1,29 @@
 module Day10 (day10a, day10b) where
 
-import Data.List (sort, tails)
 import Data.Function.Memoize (memoize)
+import Data.List (sort, tails)
+import Helper.Misc
 
 day10a :: String -> Int
 day10a xs = (*) nThrees nOnes
-    where adapters = (0:) . sort . map read $ lines xs
-          differences = zipWith (-) (tail adapters) adapters
-          nThrees = (+1) . length $ filter (==3) differences
-          nOnes = length $ filter (==1) differences
+  where
+    adapters = (0 :) . sort . map read $ lines xs
+    differences = zipWith (-) (tail adapters) adapters
+    nThrees = (+ 1) . length $ filter (== 3) differences
+    nOnes = length $ filter (== 1) differences
 
 day10b :: String -> Int
-day10b = count . reverse . (0:) . sort . map read . lines
+day10b = count . reverse . (0 :) . sort . map read . lines
 
 count :: [Int] -> Int
 count [] = 0
 count [_] = 1
 count [_, _] = 1
-count (x:xs) = sum . map count' . 
-    takeWhile ((\y -> x-y < 4) . head) .
-    takeWhile (not . null) $ 
-    tails xs
+count (x : xs) =
+  sum . map count'
+    . takeWhile ((\y -> x - y < 4) . head)
+    . takeWhile (not . null)
+    $ tails xs
 
 count' :: [Int] -> Int
 count' = memoize count
